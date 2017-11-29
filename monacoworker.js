75a721f1cae2c0b0a343d2b1e386b89f6d7fb013
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-11-28 22:04:02 -0800

    Auto-push

--- monacoworker.js
+++ monacoworker.js
@@ -1,5 +1,1 @@
-// see https://github.com/Microsoft/monaco-editor
-self.MonacoEnvironment = { 
-    baseUrl: "/pxt-sample/" 
-};
-importScripts("/pxt-sample/vs/base/worker/workerMain.js");
+self.MonacoEnvironment={baseUrl:"/pxt-sample/"},importScripts("/pxt-sample/vs/base/worker/workerMain.js");
